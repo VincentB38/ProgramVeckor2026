e8bbfff941832979adc8278f2fd3eac21cbef932
FedericoQB <federico.quijano@outlook.com>
2026-01-12 09:46:38 +0100

    Added enemy class

---
+++ ProgramV 2026/Assets/Scripts/EnemyScripts/Enemy.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    string name;
+
+    float speed;
+    Rigidbody2D rb;
+
+    public Enemy(float speed, Rigidbody2D rb)
+    {
+        this.speed = speed;
+        this.rb = rb;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Virtual function to make changes in each subclass
+    public virtual void Move(Vector3 playerPosition)
+    {
+        
+    }
+
+    // In Subclass, uses a ontrigger fn to decide layer by adding an index to each trigger
+    public void LayerMove(int layerIndex)
+    {
+        gameObject.layer = layerIndex;
+
+        // Run function to change collision layer
+    }
+
+    public virtual void Attack()
+    {
+
+    }
+
+
+}
