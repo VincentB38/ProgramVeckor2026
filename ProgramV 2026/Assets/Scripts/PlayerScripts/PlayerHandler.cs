d39e3cfa37e7ef991e6ffc6830a59176d2e9cad3
VincentB38 <vincent.blomberg@elev.ga.lbs.se>
2026-01-12 10:07:57 +0100

    Player health

--- ProgramV 2026/Assets/Scripts/PlayerScripts/PlayerHandler.cs
+++ ProgramV 2026/Assets/Scripts/PlayerScripts/PlayerHandler.cs
@@ -1,16 +1,58 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int Health = 3;
+
+    public Transform HeartContainer;
+    public GameObject HeartPreFab;
+
+    private List<RawImage> Hearts = new List<RawImage>();
     void Start()
     {
-        
+        CreateHearts();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.hKey.wasPressedThisFrame)
+        {
+            ChangeHealth(-1);
+        }
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        Health += amount;
+
+        CalculateHearts();
+    }
+
+    void CreateHearts()
+    {
+        for (int i = 0; i < Health; i++) // Skappar hjärtan baserat på health
+        {
+            GameObject heartObj = Instantiate(HeartPreFab, HeartContainer);
+            heartObj.name = "Heart" + i;
+
+            RectTransform rt = heartObj.GetComponent<RectTransform>(); // Placera Hjärtan
+            rt.anchoredPosition = new Vector2(-1158 + (178 *i), 569);
+
+            RawImage heartImage = heartObj.GetComponent<RawImage>(); // lägga til i listan
+
+            if (heartImage != null)
+                Hearts.Add(heartImage);
+        }
+    }
+
+    void CalculateHearts()
+    {
+        for (int i = 0; i < Hearts.Count; i++)
+        {
+                Hearts[i].gameObject.SetActive(i < Health);
+        }
     }
 }
