64ba484b12d41893eda5da68d92a1b8d0bbef5a6
FedericoQB <federico.quijano@outlook.com>
2026-01-15 11:11:34 +0100

    Added scene management and main menu

---
+++ ProgramV 2026/Assets/Scripts/UI/SceneManageLoader.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class SceneManageLoader : MonoBehaviour
+{
+    static public bool gameIsPaused;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.pKey.isPressed)
+        {
+            if (gameIsPaused)
+            {
+                Time.timeScale = 0f;
+                // Add pause menu
+            }
+
+            if (!gameIsPaused)
+            {
+                // Hide pause menu
+                Time.timeScale = 1f;
+            }
+        }
+    }
+
+    // Opens "TestScene"
+    public void OpenLevel1()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+}
