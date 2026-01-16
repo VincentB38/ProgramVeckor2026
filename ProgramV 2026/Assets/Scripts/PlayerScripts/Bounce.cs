0ca3cc3b10c2625988ea10f66857bcff13ec7073
VincentB38 <vincent.blomberg@elev.ga.lbs.se>
2026-01-16 13:38:38 +0100

    YES

--- ProgramV 2026/Assets/Scripts/PlayerScripts/Bounce.cs
+++ ProgramV 2026/Assets/Scripts/PlayerScripts/Bounce.cs
@@ -1,15 +1,19 @@
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 public class Bounce : MonoBehaviour
 {
     public float bouncePower;
+
+    public GameObject Player;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().linearVelocityX = -collision.gameObject.GetComponent<Rigidbody2D>().linearVelocityX * bouncePower;
+            Player.GetComponent<PlayerHandler>().EndGame("False");
         }
     }
 }
